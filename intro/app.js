3a005380a193604a60b777e7686326df30b5b960
sajicode <sajioloye@gmail.com>
2019-02-14 15:40:35 +0100

    round up typescript types

--- intro/app.js
+++ intro/app.js
@@ -1,2 +1,94 @@
 "use strict";
-console.log('Hello World, It works!');
+// string
+var myName = 'Fola';
+// myName = 27;
+// number
+var myAge = 27;
+// myAge = "Fola"
+// boolean
+var hasHobbies = true;
+// hasHobbies = 1;
+// explicit types
+var myRealAge;
+myRealAge = 26;
+// array
+var hobbies = ['Cooking', 'Gaming'];
+hobbies = [100];
+// tuples
+var address = ['Supers', 99];
+// enums - used to make numbers more expressive
+var Color;
+(function (Color) {
+    Color[Color["Gray"] = 0] = "Gray";
+    Color[Color["Green"] = 100] = "Green";
+    Color[Color["Blue"] = 101] = "Blue"; // 101
+})(Color || (Color = {}));
+var myColor = Color.Green;
+// myColor => 100;
+// any - use this rarely
+var car = 'BMW';
+car = { brand: 'BMW', series: 3 };
+// functions
+// the type string specifies what the return value should be
+function returnName() {
+    return myName;
+}
+// void - you can't return anything in a void function
+function sayHello() {
+    console.log('Hello thee');
+}
+// argument types
+function multiply(value1, value2) {
+    return value1 * value2;
+}
+// function types
+var myMultiply;
+// objects and types
+var userData = {
+    name: 'Fola',
+    age: 26
+};
+var complex = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+var complex2 = {
+    data: [100, 3.99, 10],
+    output: function (all) {
+        return this.data;
+    }
+};
+// union types
+// either a number or a string
+var myRealRealAge = 28;
+// check types
+var finalValue = 'a string';
+if (typeof finalValue === 'string') {
+    console.log('Final value is a number');
+}
+// never
+function neverReturns() {
+    throw new Error('An error');
+}
+// nullable
+var canBeNull = 12;
+canBeNull = null;
+var canAlsoBeNull;
+canAlsoBeNull = null;
+var canThisBeAny = null;
+canThisBeAny = 12;
+var bankAccount = {
+    money: 2000,
+    deposit: function (value) {
+        this.money += value;
+    }
+};
+var myself = {
+    name: 'Max',
+    bankAccount: bankAccount,
+    hobbies: ['Sports', 'Cooking']
+};
+myself.bankAccount.deposit(3000);
+console.log(myself);
--- intro/app.ts
+++ intro/app.ts
@@ -1,1 +1,123 @@
-console.log('Hello World, It works!');
+// string
+let myName = 'Fola';
+// myName = 27;
+
+// number
+let myAge = 27;
+// myAge = "Fola"
+
+// boolean
+let hasHobbies = true;
+// hasHobbies = 1;
+
+// explicit types
+let myRealAge: number;
+myRealAge = 26;
+
+// array
+let hobbies: any[] = [ 'Cooking', 'Gaming' ];
+hobbies = [ 100 ];
+
+// tuples
+let address: [string, number] = [ 'Supers', 99 ];
+
+// enums - used to make numbers more expressive
+enum Color {
+	Gray, // 0
+	Green = 100, // 100
+	Blue // 101
+}
+
+let myColor: Color = Color.Green;
+// myColor => 100;
+
+// any - use this rarely
+let car: any = 'BMW';
+car = { brand: 'BMW', series: 3 };
+
+// functions
+// the type string specifies what the return value should be
+function returnName(): string {
+	return myName;
+}
+
+// void - you can't return anything in a void function
+function sayHello(): void {
+	console.log('Hello thee');
+}
+
+// argument types
+function multiply(value1: number, value2: number): number {
+	return value1 * value2;
+}
+
+// function types
+let myMultiply: (val1: number, val2: number) => number;
+
+// objects and types
+let userData: { name: string; age: number } = {
+	name: 'Fola',
+	age: 26
+};
+// userData = {} // doesn't work
+
+// complex object
+type Complex = { data: number[]; output: (all: boolean) => number[] };
+
+let complex: { data: number[]; output: (all: boolean) => number[] } = {
+	data: [ 100, 3.99, 10 ],
+	output: function(all: boolean): number[] {
+		return this.data;
+	}
+};
+
+let complex2: Complex = {
+	data: [ 100, 3.99, 10 ],
+	output: function(all: boolean): number[] {
+		return this.data;
+	}
+};
+
+// union types
+// either a number or a string
+let myRealRealAge: number | string = 28;
+
+// check types
+let finalValue = 'a string';
+
+if (typeof finalValue === 'string') {
+	console.log('Final value is a number');
+}
+
+// never
+function neverReturns(): never {
+	throw new Error('An error');
+}
+
+// nullable
+let canBeNull: number | null = 12;
+canBeNull = null;
+let canAlsoBeNull;
+canAlsoBeNull = null;
+let canThisBeAny = null;
+canThisBeAny = 12;
+
+// Exercise
+type BankAccount = { money: number; deposit: (val: number) => void };
+
+let bankAccount: BankAccount = {
+	money: 2000,
+	deposit(value: number) {
+		this.money += value;
+	}
+};
+
+let myself: { name: string; bankAccount: BankAccount; hobbies: string[] } = {
+	name: 'Max',
+	bankAccount: bankAccount,
+	hobbies: [ 'Sports', 'Cooking' ]
+};
+
+myself.bankAccount.deposit(3000);
+
+console.log(myself);
